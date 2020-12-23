bb985963c62353236c5bb71e979bb0dee3951e5d
Thiago Fialho <45884516+thiagohbfialho@users.noreply.github.com>
2020-12-23 10:11:24 -0300

    Getting a post by id

---
+++ src/main/java/com/thiagofialho/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.thiagofialho.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagofialho.workshopmongo.domain.Post;
+import com.thiagofialho.workshopmongo.repository.PostRepository;
+import com.thiagofialho.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
